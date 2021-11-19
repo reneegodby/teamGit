99d80be617cb9ca02978ccd1f03b3a0927c89cf9
reneegodby <57304584+reneegodby@users.noreply.github.com>
2021-11-19 10:24:10 -0500

    header and footer components started. App.js in the works

---
+++ teamgit/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import Footer from './components/site/Footer';
+import Header from './components/site/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <body>test</body>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
