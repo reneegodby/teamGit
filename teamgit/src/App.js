6cf4cbe15a63bb4c62de96eda7f4de0e3f5fe8ab
reneegodby <57304584+reneegodby@users.noreply.github.com>
2021-11-19 15:41:26 -0500

    attempred NASA API. It is not finished or functional yet

--- teamgit/src/App.js
+++ teamgit/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
+// import React, {Component} from "react";
+// import {render} from "react-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import {Card, CardText, CardBody, CardTitle, CardSubtitle, Button, Container, Row, Col} from 'reactstrap';
+import {Container, Row, Col} from 'reactstrap';
 import Footer from './components/site/Footer';
 import Header from './components/site/Header';
+import NasaApi  from './components/site/Apps/NASA/NasaApi';
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           </Col>
           <Col>
           <h2>NASA API</h2>
+          <NasaApi />
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </Col>
           <Col>
