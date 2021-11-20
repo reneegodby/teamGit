eb262627cab4c3d5c2f7cafff5a5992ff4a94c3d
reneegodby <57304584+reneegodby@users.noreply.github.com>
2021-11-20 14:02:36 -0500

    need to commit again to merge branches

--- teamgit/src/components/site/Apps/NASA/NasaApi.js
+++ teamgit/src/components/site/Apps/NASA/NasaApi.js
@@ -1,29 +1,29 @@
-import React, { useState } from "react";
-import {Button} from 'reactstrap';
+import React, { useState, useEffect } from "react";
+import { Button } from "reactstrap";
 import styled from "styled-components";
 
 const Img = styled.img`
-height: 150px;
-width: 150px;
-`
+  height: 350px;
+  width: 350px;
+`;
 
 const baseUrl = "https://api.nasa.gov/planetary/earth/imagery";
 const key = "axnei6Bx1WjoXq8EXkYZRdUznhudstUwKzTFfBoE";
-// Since our base API url and the API key won't change as the users interact with the application we can declare them as const's outside of our class component.
 
 const NasaApi = (props) => {
   const [results, setResults] = useState({});
-  
-  // That should take care of all the data we need to store that can change depending on how the user interacts with our application. I'm assuming I'll use this with the location tracker.
-   
+
+  // React.useEffect(() => {
+  // window.addEventListener('load', function () {
+
   const fetchImage = () => {
     let url = `${baseUrl}?lon=${props.lon}&lat=${props.lat}&date=2014-01-01&dim=0.15&api_key=${key}`;
     fetch(url)
       .then((res) => res)
       .then((res) => {
-          console.log(res);
-          setResults(res.url);
-        })
+        console.log(res);
+        setResults(res.url);
+      })
       .catch((err) => console.log(err));
   };
 
