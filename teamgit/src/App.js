ee9d8affd40da124535c5fb4ce47d4cec31a74f8
reneegodby <57304584+reneegodby@users.noreply.github.com>
2021-11-20 12:15:12 -0500

    nasa fetch working

--- teamgit/src/App.js
+++ teamgit/src/App.js
@@ -1,12 +1,10 @@
-// import React, {Component} from "react";
-// import {render} from "react-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import {Container, Row, Col} from 'reactstrap';
 import Footer from './components/site/Footer';
 import Header from './components/site/Header';
 import NasaApi  from './components/site/Apps/NASA/NasaApi';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 
@@ -15,7 +13,7 @@ function App() {
   const [lon, setLon] = useState(null);
   const [status, setStatus] = useState(null);
 
-  const getLocation = () => {
+    const getLocation = () => {
     if (!navigator.geolocation) {
       setStatus('Geolocation is not supported by your browser');
     } else {
@@ -32,12 +30,17 @@ function App() {
   }
   // This function gets the user’s current position and accepts 3 parameters; a success callback function, an error callback function, and a position options object. Only using the callback function. 
 
+  useEffect(() => {
+    getLocation();
+  }, []);
+  
+
   return (
     <div className="App">
       <Header />
       <hr/>
       <div className="App">
-        <button onClick={getLocation}>Get Location</button>
+        
         <p>{status}</p>
         {lat && <p>Latitude: {lat}</p>}
         {lon && <p>Longitude: {lon}</p>}
@@ -51,8 +54,7 @@ function App() {
           </Col>
           <Col>
           <h2>NASA API</h2>
-          <NasaApi />
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <NasaApi lat={lat} lon={lon}/>
           </Col>
           <Col>
           <h2>Ticketmaster API</h2>
