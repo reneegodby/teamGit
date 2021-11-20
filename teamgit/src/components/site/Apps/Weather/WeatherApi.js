623ef753493359b50e9c29a9d4e75731539bc8aa
AlaynaCarlin <alaynacarlin18@gmail.com>
2021-11-20 12:20:06 -0500

    fetch working

--- teamgit/src/components/site/Apps/Weather/WeatherApi.js
+++ teamgit/src/components/site/Apps/Weather/WeatherApi.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import { Button, Label, Input } from 'reactstrap'
 
-const baseUrl = 'api.openweathermap.org/data/2.5/weather';
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 const key = '16bfd0882021f8b50da9acac7d8ee70d';
 
 const WeatherApi = (props) => {
@@ -9,11 +9,10 @@ const WeatherApi = (props) => {
     const [results, setResults] = useState({});
 
     const fetchWeather = () => {
-        let url = `${baseUrl}?lat=35&lon=139&appid=${key}`;
+        let url = `${baseUrl}?lat=41.1306&lon=-85.12886&appid=${key}`;
 
         fetch (url)
-            .then((res) => {
-                res.json() })
+            .then(res => res.json())
             .then(data => console.log(data))
             .catch(err => console.log(err));
     }
